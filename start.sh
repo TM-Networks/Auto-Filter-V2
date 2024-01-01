ed3d8a1773d78b39a7ba9b4cd80c0fa11f5ecd6d
TM-Networks <155299360+TM-Networks@users.noreply.github.com>
2024-01-01 06:52:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/afluaflu123/Eva-bot.git /Eva-bot
+  git clone https://github.com/TM-Networks/Auto-Filter-V2t.git /Auto-Filter-V2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva-bot
+  git clone $UPSTREAM_REPO /Auto-Filter-V2
 fi
-cd /Eva-bot
+cd /Auto-Filter-V2
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Auto-Filter-V2...."
 python3 bot.py
